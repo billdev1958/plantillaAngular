7ce17f301841656d1dc8fb3815eac11f75d1687f
billy.rivera <briveras001@uaemex.mx>
2025-05-27 16:28:22 -0600

    first commit

---
+++ backend-api/src/main/java/com/uaemex/backend_api/BackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.uaemex.backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiApplication.class, args);
+	}
+
+}
